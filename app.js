4d4a4b4eb16856df2a758caaa1fac2d0d1d871da
Mesut Özsoy <mesut.776@gmail.com>
2019-10-30 04:19:07 +0300

    Merhaba Dünya!
    Vue İlk Başlangıç

---
+++ app.js
@@ -0,0 +1,10 @@
+window.vue = new Vue({
+    el: '#app',
+    name: 'helloword',
+    data: {
+      message: 'Merhaba Dunya!',
+      messageforhtml: '<h1 style="color:red;">h1 etiket ile Merhaba Dünya!</h1>',
+      isLoggedIn: true,
+      username: 'mesut'
+    }
+  })
